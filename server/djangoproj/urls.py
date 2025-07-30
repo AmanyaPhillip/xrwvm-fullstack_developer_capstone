c41d9b42346310b24701a0f6f1a11ef18941cd32
AmanyaPhillip <amanyaphillip@outlook.com>
2025-07-30 00:42:40 -0400

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,7 +1,8 @@
-"""djangoproj URL Configuration
+"""
+URL configuration for djangoproj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,20 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path, include # E231: missing whitespace after ',' ; E501: Line too long.
+from django.conf import settings # Add these for static files in development
+from django.conf.urls.static import static # Add these for static files in development
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('djangoapp/', include('djangoapp.urls')), # E231: missing whitespace after ',' ; E501: Line too long.
+    path('', include('djangoapp.urls')), # This makes your 'index' route accessible at the root
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
