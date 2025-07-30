3d32909f595d60214ee6f53cf11eb0552e979c3b
AmanyaPhillip <amanyaphillip@outlook.com>
2025-07-30 00:59:44 -0400

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,29 +1,34 @@
-from .models import CarMake, CarModel # E302: Added 1 blank line
-from django.db.models import F
+from .models import CarMake, CarModel  # E302: Added 1 blank line
+#from django.db.models import F
 
 def initiate():
-    # E231: Missing whitespace after ':'. Add space after colons in dictionary literals.
-    # E501: Break long lines.
     car_make_data = [
         {"name": "Nissan", "description": "Great cars. Japanese technology"},
-        {"name": "Mercedes-Benz", "description": "Great cars. German technology"},
+        {"name": "Mercedes-Benz",
+         "description": "Great cars. German technology"},
         {"name": "Audi", "description": "Great cars. German technology"},
         {"name": "Kia", "description": "Great cars. Korean technology"},
         {"name": "Honda", "description": "Great cars. Japanese technology"},
     ]
 
-    for data in car_make_data: # E117: This loop should not be over-indented.
-        CarMake.objects.create(name=data['name'], description=data['description']) # E501: Line too long.
+    for data in car_make_data:  # E117: This loop should not be over-indented.
+        CarMake.objects.create(
+            name=data['name'], description=data['description'])  # E501: Line too long.
 
     # E303: Remove extra blank line.
     # E231, E501: Add whitespace after colons and break long lines.
     car_model_data = [
-        {"car_make": "Nissan", "name": "Pathfinder", "type": "SUV", "year": 2023},
-        {"car_make": "Nissan", "name": "Frontier", "type": "Truck", "year": 2023},
+        {"car_make": "Nissan", "name": "Pathfinder",
+         "type": "SUV", "year": 2023},
+        {"car_make": "Nissan", "name": "Frontier",
+         "type": "Truck", "year": 2023},
         {"car_make": "Nissan", "name": "Leaf", "type": "Sedan", "year": 2023},
-        {"car_make": "Mercedes-Benz", "name": "C-Class", "type": "Sedan", "year": 2023},
-        {"car_make": "Mercedes-Benz", "name": "E-Class", "type": "Sedan", "year": 2023},
-        {"car_make": "Mercedes-Benz", "name": "GLC SUV", "type": "SUV", "year": 2023},
+        {"car_make": "Mercedes-Benz", "name": "C-Class",
+         "type": "Sedan", "year": 2023},
+        {"car_make": "Mercedes-Benz", "name": "E-Class",
+         "type": "Sedan", "year": 2023},
+        {"car_make": "Mercedes-Benz", "name": "GLC SUV",
+         "type": "SUV", "year": 2023},
         {"car_make": "Audi", "name": "A4", "type": "Sedan", "year": 2023},
         {"car_make": "Audi", "name": "Q5", "type": "SUV", "year": 2023},
         {"car_make": "Audi", "name": "e-tron", "type": "SUV", "year": 2023},
@@ -35,7 +40,7 @@ def initiate():
         {"car_make": "Honda", "name": "Pilot", "type": "SUV", "year": 2023},
     ]
 
-    for data in car_model_data: # E117: This loop should not be over-indented.
+    for data in car_model_data:  # E117: This loop should not be over-indented.
         # E131: Align continuation line for hanging indent.
         car_make_instance = CarMake.objects.get(name=data['car_make'])
         CarModel.objects.create(
@@ -43,6 +48,6 @@ def initiate():
             name=data['name'],
             car_type=data['type'],
             year=data['year']
-        ) # E501: Line too long. Break into multiple lines for readability.
+        )  # E501: Line too long. Break into multiple lines for readability.
 
 # W292: Add a newline at the very end of the file.
